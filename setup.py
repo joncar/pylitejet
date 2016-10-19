5fd1588d84115b80455977234cba77375663bd10
Jon Caruana <jon@joncaruana.com>
2016-10-19 03:29:14 +0000

    Last minute changes submitting to PyPI.

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'pylitejet',
   packages = ['pylitejet'],
-  version = '0.1',
+  version = '0.1.0',
   description = 'A library for controlling a LiteJet lighting system.',
   author = 'Jon Caruana',
   author_email = 'jon@joncaruana.com',
   url = 'https://github.com/joncar/pylitejet',
-  download_url = 'https://github.com/joncar/pylitejet/tarball/0.1',
+  download_url = 'https://github.com/joncar/pylitejet/tarball/0.1.0',
   keywords = ['litejet'],
-  classifiers = [],
-  license = 'MIT'
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.4',
+  ],
+  license = 'MIT',
+  install_requires = ['pyserial']
   )
