130d754a629e70f42fdb8c79cfda99644a71b665
Jon Caruana <jon@joncaruana.com>
2020-12-15 07:25:08 +0000

    Accept auto-formatter changes.

--- test_handlers/protocol_ljtest.py
+++ test_handlers/protocol_ljtest.py
@@ -4,6 +4,7 @@ import threading
 
 _LOGGER = logging.getLogger(__name__)
 
+
 class Serial(SerialBase):
     def open(self):
         self._read_buffer = bytes()
@@ -17,7 +18,7 @@ class Serial(SerialBase):
         self.is_open = False
 
     def from_url(self, url):
-        _LOGGER.error('url is %s', url)
+        _LOGGER.error("url is %s", url)
 
     @property
     def in_waiting(self):
@@ -45,55 +46,56 @@ class Serial(SerialBase):
 
     def _set_load(self, number, level):
         self._load_levels[number] = level
-        self._respond('^K{0:03d}{1:02d}\r'.format(number, level))
+        self._respond("^K{0:03d}{1:02d}\r".format(number, level))
 
     def _set_switch(self, number, pressed):
         if self._switch_pressed.get(number, False) == pressed:
             return
         self._switch_pressed[number] = pressed
         if pressed:
-            event = 'P'
+            event = "P"
         else:
-            event = 'R'
-        self._respond('{0}{1:03d}\r'.format(event, number))
+            event = "R"
+        self._respond("{0}{1:03d}\r".format(event, number))
 
     def write(self, data):
-        str = data.decode('utf-8')
-        assert str[0] == '^'
+        str = data.decode("utf-8")
+        assert str[0] == "^"
         command = str[1]
-        if command != 'G' and command != 'H':
+        if command != "G" and command != "H":
             number = int(str[3:5])
-        if command == 'A':
+        if command == "A":
             self._set_load(number, 99)
-        elif command == 'B':
+        elif command == "B":
             self._set_load(number, 0)
-        elif command == 'C' or command == 'D':
-            _LOGGER.warning('Scenes not supported')
-        elif command == 'E':
+        elif command == "C" or command == "D":
+            _LOGGER.warning("Scenes not supported")
+        elif command == "E":
             level = int(str[6:8])
             rate = int(str[8:10])
             self._set_load(number, level)
-        elif command == 'F':
-            self._respond('{0:02d}\r'.format(self._load_levels.get(number, 0)))
-        elif command == 'G':
-            _LOGGER.warning('Instant status not supported')
-            self._respond('000000000000000000000000000000000000000000000000\r')
-        elif command == 'H':
-            _LOGGER.warning('Instant status not supported')
-            self._respond('00000000000000000000000000000000000000 0000000000000000000000000000000000000000000000000000000000\r')
-        elif command == 'I':
+        elif command == "F":
+            self._respond("{0:02d}\r".format(self._load_levels.get(number, 0)))
+        elif command == "G":
+            _LOGGER.warning("Instant status not supported")
+            self._respond("000000000000000000000000000000000000000000000000\r")
+        elif command == "H":
+            _LOGGER.warning("Instant status not supported")
+            self._respond(
+                "00000000000000000000000000000000000000 0000000000000000000000000000000000000000000000000000000000\r"
+            )
+        elif command == "I":
             self._set_switch(number, True)
-        elif command == 'J':
+        elif command == "J":
             self._set_switch(number, False)
-        elif command == 'K':
-            self._respond('Switch #{}\r'.format(number))
-        elif command == 'L':
-            self._respond('Load #{}\r'.format(number))
-        elif command == 'M':
-            self._respond('Scene #{}\r'.format(number))
+        elif command == "K":
+            self._respond("Switch #{}\r".format(number))
+        elif command == "L":
+            self._respond("Load #{}\r".format(number))
+        elif command == "M":
+            self._respond("Scene #{}\r".format(number))
         return len(data)
 
     def _respond(self, str):
-        self._read_buffer = str.encode('utf-8')
+        self._read_buffer = str.encode("utf-8")
         self._read_ready.set()
-
