4b53b207f2fc7531f9ff8979e197f431df4b6663
Jon Caruana <jon@joncaruana.com>
2022-12-30 00:20:24 +0000

    Update versions and add publish workflow.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name="pylitejet",
     packages=["pylitejet"],
-    version="0.4.4",
+    version="0.4.5",
     description="A library for controlling a LiteJet lighting system.",
     author="Jon Caruana",
     author_email="jon@joncaruana.com",
     url="https://github.com/joncar/pylitejet",
-    download_url="https://github.com/joncar/pylitejet/tarball/0.3.0",
+    download_url="https://github.com/joncar/pylitejet/tarball/0.4.5",
     keywords=["litejet"],
     classifiers=[
         "Development Status :: 3 - Alpha",
