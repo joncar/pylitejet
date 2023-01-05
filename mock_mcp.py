748344eaaaaf5790949b144555c4bb605007e180
Jon Caruana <jon@joncaruana.com>
2023-01-04 22:17:04 -0800

    Expose mock MCP over TCP for easy testing with Home Assistant.

---
+++ mock_mcp.py
@@ -0,0 +1,93 @@
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+class MockMCP:
+    def __init__(self):
+        self._load_levels = {}
+        self._switch_pressed = {}
+        self._broadcast_receivers = []
+
+    def _broadcast(self, line: str):
+        b = line.encode('utf-8')
+        _LOGGER.info(f"bcst MCP: {b}")
+        _LOGGER.info(self._broadcast_receivers)
+        for r in self._broadcast_receivers:
+            r(b)
+
+    def add_listener(self, r):
+        self._broadcast_receivers.append(r)
+
+    def set_load(self, number: int, level: int):
+        self._load_levels[number] = level
+        self._broadcast(f"^K{number:03d}{level:02d}\r")
+
+    def set_switch(self, number: int, pressed: bool):
+        if self._switch_pressed.get(number, False) == pressed:
+            return
+        self._switch_pressed[number] = pressed
+        if pressed:
+            event = "P"
+        else:
+            event = "R"
+        self._broadcast(f"{event}{number:03d}\r")
+
+    def handle_input(self, data: bytes):
+        _LOGGER.info(f"  to MCP: {data}")
+        str = data.decode("utf-8")
+
+        # Skip until a command start marker
+        start_index = 0
+        while start_index < len(str) and str[start_index] != '+':
+            start_index += 1
+        if start_index != 0:
+            _LOGGER.debug(f"Skipped {start_index} bytes")
+            return start_index, None
+
+        command = str[1]
+        response = None
+        if command != "G" and command != "H":
+            number = int(str[2:5])
+        if command == "A":
+            self.set_load(number, 99)
+            command_length = 5
+        elif command == "B":
+            self.set_load(number, 0)
+            command_length = 5
+        elif command == "C" or command == "D":
+            _LOGGER.warning("Scenes not supported")
+        elif command == "E":
+            level = int(str[5:7])
+            rate = int(str[7:9])
+            self.set_load(number, level)
+            command_length = 9
+        elif command == "F":
+            response = f"{self._load_levels.get(number, 0):02d}\r"
+            command_length = 5
+        elif command == "G":
+            _LOGGER.warning("Instant status not supported")
+            response = "000000000000000000000000000000000000000000000000\r"
+            command_length = 2
+        elif command == "H":
+            _LOGGER.warning("Instant status not supported")
+            response = "00000000000000000000000000000000000000 0000000000000000000000000000000000000000000000000000000000\r"
+            command_length = 2
+        elif command == "I":
+            self.set_switch(number, True)
+            command_length = 5
+        elif command == "J":
+            self.set_switch(number, False)
+            command_length = 5
+        elif command == "K":
+            response = "Switch #{number}\r"
+            command_length = 5
+        elif command == "L":
+            response = f"Load #{number}\r"
+            command_length = 5
+        elif command == "M":
+            response = f"Scene #{number}\r"
+            command_length = 5
+        if response:
+            response = response.encode('utf-8')
+            _LOGGER.info(f"from MCP: {response}")
+        return command_length, response
---
+++ mock_server.py
@@ -0,0 +1,39 @@
+import asyncio
+import logging
+from mock_mcp import MockMCP
+
+_LOGGER = logging.getLogger(__name__)
+
+async def handle_client(reader, writer):
+    _LOGGER.info("Client connected")
+    loop = asyncio.get_running_loop()
+
+    async def async_write(data):
+        writer.write(data)
+        await writer.drain()
+    def handle_broadcast(data):
+        asyncio.run_coroutine_threadsafe(async_write(data), loop)
+    mcp.add_listener(handle_broadcast)
+
+    while True:
+        request = await reader.readuntil(separator=b'\n')
+        while len(request) > 0:
+            length, response = mcp.handle_input(request)
+            if response:
+                await async_write(response)
+            request = request[length:]
+    writer.close()
+    _LOGGER.info("Client disconnected")
+
+async def run_server():
+    port = 9999
+    _LOGGER.info(f"Listening for connections on port {port}")
+    server = await asyncio.start_server(handle_client, port=port)
+    async with server:
+        await server.serve_forever()
+
+logging.basicConfig(level=logging.DEBUG)
+
+mcp = MockMCP()
+
+asyncio.run(run_server())
